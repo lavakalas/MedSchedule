6cf58b0580b7cf40ea1b3a8b0323cb870698f3be
lavakalas <lavakalas@mail.ru>
2022-10-22 21:32:55 +0200

    Created basic DBAdapter class with SELECT functionality

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-pass
+import sqlite3
+
+
+class AdapterDB:
+    def __init__(self, db):
+        self.db = db
+        self.con = sqlite3.connect(self.db)
+        self.cur = self.con.cursor()
+
+    def get_all_from(self, table):
+        sqlReq = f"""SELECT * FROM {table}"""
+        return self.cur.execute(sqlReq).fetchall()
+
+    def get_selective(self, table, condition):
+        sqlReq = f"""SELECT * FROM {table} WHERE {condition}"""
+        return self.cur.execute(sqlReq).fetchall()
