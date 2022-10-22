4e036b7f7cf9ee1fbd2aabdc45acfc3c3eec56cf
lavakalas <lavakalas@mail.ru>
2022-10-22 22:25:05 +0200

    Небольшой тест в main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+from pprint import pprint
 
 
 class AdapterDB:
@@ -14,3 +15,28 @@ class AdapterDB:
     def get_selective(self, table, condition):
         sqlReq = f"""SELECT * FROM {table} WHERE {condition}"""
         return self.cur.execute(sqlReq).fetchall()
+
+
+if __name__ == '__main__':
+    RoomsDB = AdapterDB("Rooms.db")
+    pprint(RoomsDB.get_all_from('rooms'))
+
+    con = sqlite3.connect("Rooms.db")
+    cur = con.cursor()
+
+    sql = """CREATE TABLE IF NOT EXISTS rooms("id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, "number" int)"""
+    cur.execute(sql)
+
+    cur.execute("""DELETE FROM rooms""")  # Очищает существующую таблицу
+
+    cur.execute("""DELETE FROM sqlite_sequence WHERE name = "rooms" """)  # обнуляет счётчик для id (автоинкременция)
+
+    sql1 = "INSERT INTO rooms(number) VALUES"  # заготовка для вставления новых значений
+    for i in range(101, 201):
+        sql1 += f"({i}), "
+    else:
+        sql1 = sql1[:-2]  # создание полного запроса
+    cur.execute(sql1)
+
+    con.commit()
+    con.close()
--- scratch.py
+++ scratch.py
@@ -4,16 +4,14 @@ con = sqlite3.connect("Rooms.db")
 cur = con.cursor()
 sql = """CREATE TABLE IF NOT EXISTS rooms("id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, "number" int)"""
 cur.execute(sql)
-cur.execute("""DELETE FROM rooms""")
-cur.execute("""DELETE FROM sqlite_sequence WHERE name = "rooms" """)
-sql1 = "INSERT INTO rooms(number) VALUES"
+cur.execute("""DELETE FROM rooms""")  # Очищает существующую таблицу
+cur.execute("""DELETE FROM sqlite_sequence WHERE name = "rooms" """)  # обнуляет счётчик для id (автоинкременция)
+sql1 = "INSERT INTO rooms(number) VALUES"  # заготовка для вставления новых значений
 for i in range(101, 201):
     sql1 += f"({i}), "
 else:
-    sql1 = sql1[:-2]
+    sql1 = sql1[:-2]  # создание полного запроса
 cur.execute(sql1)
 con.commit()
 
 con.close()
-
-# Little UPDATE
